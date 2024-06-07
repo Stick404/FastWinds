c4a8d27883c064afc09716c00d685b13a7b9e8f9
Stickia <stick_man@mindlesstoys.com>
2024-06-06 20:18:52 -0400

    First Commit

---
+++ src/client/java/stickia/github/fastwinds/FastWindsClient.java
@@ -0,0 +1,10 @@
+package stickia.github.fastwinds;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FastWindsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
